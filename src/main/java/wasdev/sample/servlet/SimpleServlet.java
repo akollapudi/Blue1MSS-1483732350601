bd7c207c572db9f2a33fb1abcf33fd233ea4168b
akollapudi <akollapudi@miraclesoft.com>
2017-01-09 12:33:31 -0500

    test1

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -17,6 +17,7 @@ public class SimpleServlet extends HttpServlet {
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
