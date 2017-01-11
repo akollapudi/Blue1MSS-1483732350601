cc2ca11ccd326b95aed925ee9b67488503bb790a
akollapudi <akollapudi@miraclesoft.com>
2017-01-11 06:14:35 -0500

    Update SimpleServlet.java

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -6,10 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import wasdev.sample.functions.*;
 
 /**
- * Servlet implementation class SimpleServlet
+ * Servlet implementation class SimpleServlet..
+  Servlet implementation class SimpleServlet
+  New comment has been added.
  */
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
@@ -17,7 +19,6 @@ public class SimpleServlet extends HttpServlet {
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
